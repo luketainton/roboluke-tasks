70ac601064b8183a02f351598071d7311d65926a
Luke Tainton <luke@tainton.uk>
2024-08-30 19:31:13 +0100

    chore(lint): fix C0116

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -6,15 +6,18 @@ from app.utils.helpers import validate_email_syntax  # pragma: no cover
 
 
 def test_validate_email_syntax_true() -> None:
+    """Test validate_email_syntax() with a real email address."""
     email: str = "test@test.com"
     assert validate_email_syntax(email)
 
 
 def test_validate_email_syntax_false1() -> None:
+    """Test validate_email_syntax() with an invalid email address."""
     email: str = "test@test"
     assert not validate_email_syntax(email)
 
 
 def test_validate_email_syntax_false2() -> None:
+    """Test validate_email_syntax() with an invalid email address."""
     email: str = "test"
     assert not validate_email_syntax(email)
