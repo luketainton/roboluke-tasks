ff6dd431e1b04d781c7b3f19ef63ffd24b79ea8c
Luke Tainton <luke@tainton.uk>
2024-11-21 22:24:15 +0000

    fix: add unit test for non-existent env vars

--- tests/test_config_1.py
+++ tests/test_config_1.py
@@ -6,30 +6,30 @@
 
 import os
 
-config_vars: dict = {
-    "APP_VERSION": "dev",
-    "BOT_NAME": "TestBot",
-    "WEBEX_API_KEY": "testing",
-    "ADMIN_FIRST_NAME": "Test",
-    "ADMIN_EMAIL": "test@test.com",
-    "N8N_WEBHOOK_URL": "https://n8n.test.com/webhook/abcdefg",
-    "SENTRY_ENABLED": "false",
-    "SENTRY_DSN": "http://localhost",
-    "APPROVED_USERS": "test@test.com",
-    "APPROVED_DOMAINS": "test.com",
-    "APPROVED_ROOMS": "test",
-}
-
-
-for config_var, value in config_vars.items():
-    os.environ[config_var] = value
-
-# needs to be imported AFTER environment variables are set
-from app.utils.config import config  # pragma: no cover
-
 
 def test_config() -> None:
     """Test config module."""
+
+    config_vars: dict = {
+        "APP_VERSION": "dev",
+        "BOT_NAME": "TestBot",
+        "WEBEX_API_KEY": "testing",
+        "ADMIN_FIRST_NAME": "Test",
+        "ADMIN_EMAIL": "test@test.com",
+        "N8N_WEBHOOK_URL": "https://n8n.test.com/webhook/abcdefg",
+        "SENTRY_ENABLED": "false",
+        "SENTRY_DSN": "http://localhost",
+        "APPROVED_USERS": "test@test.com",
+        "APPROVED_DOMAINS": "test.com",
+        "APPROVED_ROOMS": "test",
+    }
+
+    for config_var, value in config_vars.items():
+        os.environ[config_var] = value
+
+    # needs to be imported AFTER environment variables are set
+    from app.utils.config import config  # pragma: no cover
+
     assert config.admin_emails == config_vars["ADMIN_EMAIL"].split(",")
     assert config.admin_first_name == config_vars["ADMIN_FIRST_NAME"]
     assert config.approved_domains == config_vars["APPROVED_DOMAINS"].split(",")
@@ -47,3 +47,6 @@ def test_config() -> None:
         assert config.sentry_dsn == config_vars["SENTRY_DSN"]
     else:
         assert config.sentry_dsn == ""
+
+    for config_var in config_vars:
+        os.environ.pop(config_var, None)
